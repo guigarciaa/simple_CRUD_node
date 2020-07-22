8ae8cce381c31b15166e43f9bb53d0bf1ac83d61
Guilherme Garcia <guigarcia.a@outlook.com>
2020-07-21 21:57:00 -0300

    add validation in post method.

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,59 +1,89 @@
-'use strict';
+"use strict";
 
 const mongoose = require("mongoose");
-const Product = mongoose.model('Product');
+const Product = mongoose.model("Product");
+const validationContract = require("../validators/fluent-validator");
 
 exports.get = (req, res, nex) => {
-    Product.find({
-        active: true
-    }, 'title price slug')
-        .then(data => {
+    Product.find(
+        {
+            active: true,
+        },
+        "title price slug"
+    )
+        .then((data) => {
             res.status(200).send(data);
-        }).catch(e => {
+        })
+        .catch((e) => {
             res.status(400).send(e);
         });
-}
+};
 
 exports.getBySlug = (req, res, nex) => {
-    Product.findOne({
-        slug: req.params.slug,
-        active: true
-    }, 'title description price slug tags')
-        .then(data => {
+    Product.findOne(
+        {
+            slug: req.params.slug,
+            active: true,
+        },
+        "title description price slug tags"
+    )
+        .then((data) => {
             res.status(200).send(data);
-        }).catch(e => {
+        })
+        .catch((e) => {
             res.status(400).send(e);
         });
-}
+};
 
 exports.getById = (req, res, nex) => {
     Product.findById(req.params.id)
-        .then(data => {
+        .then((data) => {
             res.status(200).send(data);
-        }).catch(e => {
+        })
+        .catch((e) => {
             res.status(400).send(e);
         });
-}
+};
 
 exports.getByTag = (req, res, nex) => {
-    Product.find({
-        tags: req.params.tag,
-        active: true
-    }, 'title description price slug tags')
-        .then(data => {
+    Product.find(
+        {
+            tags: req.params.tag,
+            active: true,
+        },
+        "title description price slug tags"
+    )
+        .then((data) => {
             res.status(200).send(data);
-        }).catch(e => {
+        })
+        .catch((e) => {
             res.status(400).send(e);
         });
-}
+};
 
 exports.post = (req, res, next) => {
+    let contract = new validationContract();
+    contract.hasMinLen(req.body.title, 3, 'O título deve conter pelo menos 3 caracteres.');
+    contract.hasMinLen(req.body.slug, 3, 'O slug deve conter pelo menos 3 caracteres.');
+    contract.hasMinLen(req.body.description, 3, 'A descrição deve conter pelo menos 3 caracteres.');
+
+    // If datas are invalids
+    if(!contract.isValid()) {
+        res.status(400).send(contract.errors()).end();
+        return;
+    }
+
     var product = new Product(req.body);
-    product.save().then(x => {
-        res.status(201).send({ message: 'Produto cadastrado com sucesso!' });
-    }).catch(e => {
-        res.status(400).send({ message: 'Falha ao cadastrar o produto!', data: e });
-    });
+    product
+        .save()
+        .then((x) => {
+            res.status(201).send({ message: "Produto cadastrado com sucesso!" });
+        })
+        .catch((e) => {
+            res
+                .status(400)
+                .send({ message: "Falha ao cadastrar o produto!", data: e });
+        });
 };
 
 exports.put = (req, res, next) => {
@@ -62,34 +92,34 @@ exports.put = (req, res, next) => {
             title: req.body.title,
             description: req.body.description,
             slug: req.body.slug,
-            price: req.body.price
-        }
+            price: req.body.price,
+        },
     })
-        .then(x => {
+        .then((x) => {
             res.status(201).send({
-                message: 'Produto atualizado com sucesso!'
+                message: "Produto atualizado com sucesso!",
             });
         })
-        .catch(e => {
+        .catch((e) => {
             res.status(400).send({
-                message: 'Falha ao atualizar produto',
-                data: e
+                message: "Falha ao atualizar produto",
+                data: e,
             });
         });
 };
 
 exports.delete = (req, res, next) => {
-    console.log(req.body.id)
+    console.log(req.body.id);
     Product.findOneAndRemove(req.body.id)
-        .then(x => {
+        .then((x) => {
             res.status(200).send({
-                message: 'Produto removido com sucesso!'
+                message: "Produto removido com sucesso!",
             });
         })
-        .catch(e => {
+        .catch((e) => {
             res.status(400).send({
-                message: 'Falha ao remover produto',
-                data: e
+                message: "Falha ao remover produto",
+                data: e,
             });
         });
 };
